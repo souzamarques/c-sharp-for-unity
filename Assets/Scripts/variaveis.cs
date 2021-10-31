7121fdbb8a86537dab392c34f0f1c8ec7666887a
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-31 10:25:01 -0300

    First Commit

---
+++ Assets/Scripts/variaveis.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variaveis : MonoBehaviour {
+
+    int vidaPlayer = 10, ataqueEnemy = 5;
+    float jumpForce = 5.5f;
+    bool mortePlayer = false;
+    string nomePlayer = "Hero";
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
