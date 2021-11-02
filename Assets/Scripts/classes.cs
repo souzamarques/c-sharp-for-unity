6ab637caec9d0481359de7c12a5142d931f4ffd7
Lucas Souza <lucassouzzamarques@gmail.com>
2021-11-02 03:24:17 -0300

    Objetos & Instância da Classe

--- Assets/Scripts/classes.cs
+++ Assets/Scripts/classes.cs
@@ -3,33 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class classes : MonoBehaviour {
+
+    Vampiros Dracula = new Vampiros();
+
     // Start is called before the first frame update
     void Start() {
-        
+        Dracula.forca = 100;
+        Dracula.velocidade = 100;
     }
 
     // Update is called once per frame
     void Update() {
+        Dracula.ataque();
+    }
+}
+
+class Vampiros {
+    public int forca;
+    public int velocidade;
+
+    public void ataque() {
         
     }
 }
 
-class vampiros : MonoBehaviour {
-    int forca;
-    int ataque;
-    int velocidade;
-
-    public void setAtaque(int ataque) {
-        this.ataque = ataque;
-    }
-}
-
-class lobisomens : vampiros {
+class Lobisomens : Vampiros {
 
     private void Start() {
         int ataque = 10;
-        setAtaque(ataque);
     }   
-
-
 }
